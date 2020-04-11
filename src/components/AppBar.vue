f7e4195257200dfc835490933446364b012aea71
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2020-04-12 01:56:42 +0900

    add vuetify and update app bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app color="black" dark flat>
+    <div v-for="(content, index) in contents" :key="index">
+      <v-btn :href="content.url" target="_blank" text>
+        <span class="mr-2">{{ content.text }}</span>
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+
+  data: () => ({
+    contents: [
+      {
+        text: "Top",
+        url: "https://github.com"
+      },
+      {
+        text: "About",
+        url: "https://github.com"
+      },
+      {
+        text: "Skills",
+        url: "https://github.com"
+      },
+      {
+        text: "Works",
+        url: "https://github.com"
+      },
+      {
+        text: "Posts",
+        url: "https://github.com"
+      }
+    ]
+  })
+};
+</script>
